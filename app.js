1298c6c138c314e36a7ec2dd78679546c7490481
leosilvadev <leosilvadev@gmail.com>
2015-08-25 13:50:55 -0300

    .

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ load('controllers')
 	.then('routes')
 	.into(app);
 
-app.listen(3000, function(){
+var port = (process.env.PORT || 5000);
+
+app.listen(port, function(){
 	console.log('Online...');
 });
 
